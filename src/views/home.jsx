a62bbda1ba947a763550e5f76bc36674b688da86
Marianvsf <marianvsf@gmail.com>
2025-10-27 00:33:16 -0400

    deploy

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import HeaderContent from "../components/headerContent";
+import HeaderContent from "../components/headerContent.jsx";
 
 function Home() {
   return (
