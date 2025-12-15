14f4f035d01553f8e29d99501782baf203086e6b
Marianvsf <marianvsf@gmail.com>
2025-12-14 22:14:05 -0400

    size container

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -9,17 +9,17 @@ function Home() {
         ¡Te damos la Bienvenida a Nuestra Aplicación!
         <small className="ms-2 font-semibold text-gray-500 dark:text-gray-400"></small>
       </h1>
-      <div className="sm:container sm:max-h-0 sm:mx-auto sm:max-w-md sm:px-6 lg:max-w-md lg:px-8 flex flex-col text-center items-center justify-center space-y-6">
+      <div className="sm:container sm:mx-auto sm:max-w-md sm:px-6 lg:max-w-md lg:px-8 flex flex-col text-center items-center justify-center space-y-6 pb-10">
         <p className="mt-7">
           Por favor, selecciona una de las siguientes opciones para continuar.
         </p>
-        <div className="flex w-full">
-          <button className="mt-2 mx-6 w-full py-2 px-1 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white uppercase bg-teal-400 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition duration-150 ease-in-out">
+        <div className="flex w-full gap-4 px-6">
+          <button className="w-full py-2 px-1 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white uppercase bg-teal-400 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition duration-150 ease-in-out">
             <Link to="/register" className="nav-link">
               Registrarse
             </Link>
           </button>
-          <button className="mt-2 w-full py-2 px-1 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white uppercase bg-teal-400 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition duration-150 ease-in-out">
+          <button className="w-full py-2 px-1 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white uppercase bg-teal-400 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition duration-150 ease-in-out">
             <Link to="/login" className="nav-link">
               Iniciar Sesión
             </Link>
