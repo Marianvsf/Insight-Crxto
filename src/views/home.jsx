2605f22dac63fafe83962d0b634c521184bcebd1
Marianvsf <marianvsf@gmail.com>
2025-12-15 21:13:45 -0400

    margin

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,11 +5,11 @@ function Home() {
   return (
     <>
       <HeaderContent />
-      <div className="sm:container sm:mx-auto sm:max-w-md sm:px-6 lg:max-w-md lg:px-8 flex flex-col text-center items-center justify-center space-y-6 pb-10">
+      <div className="mt-10 sm:container sm:mx-auto sm:max-w-md sm:px-6 lg:max-w-md lg:px-8 flex flex-col text-center items-center justify-center space-y-6 pb-10">
         <p className="text-2xl">
           Por favor, selecciona una de las siguientes opciones para continuar.
         </p>
-        <div className="flex w-full gap-4 px-6">
+        <div className="flex w-full gap-4 px-6 mt-5">
           <button className="w-full py-2 px-1 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white uppercase bg-teal-400 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition duration-150 ease-in-out">
             <Link to="/register" className="nav-link">
               Registrarse
