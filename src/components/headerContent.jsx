f1591310940be11eff1a4210c06743d2b57252e0
Marianvsf <marianvsf@gmail.com>
2025-12-15 21:09:05 -0400

    text

--- src/components/headerContent.jsx
+++ src/components/headerContent.jsx
@@ -29,7 +29,7 @@ const HeaderContent = () => {
       {images.map((image, index) => (
         <div
           key={index}
-          className={`text-center absolute inset-0 transition-opacity duration-700 ease-in-out ${
+          className={`flex flex-col justify-center items-center text-center absolute inset-0 transition-opacity duration-700 ease-in-out ${
             index === currentSlide ? "opacity-100 z-20" : "opacity-0 z-10"
           }`}
           style={{
@@ -42,11 +42,11 @@ const HeaderContent = () => {
             alt={`Slide ${index + 1}`}
           />
           <div className="absolute w-full text-center m-auto inset-0 bg-black/50 backdrop-blur-[2px]" />
-          <div className="relative z-10 w-full max-w-[980px] px-6 items-center text-white mt-16">
+          <div className="relative z-10 w-full max-w-[980px] px-6 items-center text-white">
             <h1 className="text-center m-auto relative text-3xl md:text-7xl font-extrabold tracking-tight mb-6 animate-in fade-in slide-in-from-bottom-6 duration-700 delay-100 leading-[1.1]">
               ¡Te damos la Bienvenida a Nuestra Aplicación! <br />
               <small className="ms-2 font-semibold text-gray-500 dark:text-gray-400">
-                ¿¿Listo para comen zar??
+                ¿¿Listo para comenzar??
               </small>
             </h1>
           </div>
