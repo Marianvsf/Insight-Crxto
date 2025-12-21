1c1e7d8fb3e4cff84de85a9ba0b7a72faed73bd8
Marianvsf <marianvsf@gmail.com>
2025-12-21 19:06:28 -0400

    size screen

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -55,7 +55,7 @@ export default function LoginView() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-80px)] p-4 md:p-6 bg-white">
+    <div className="flex items-center justify-center min-h-[calc(100vh-160px)] p-4 md:p-6 bg-white">
       {/* TARJETA FLOTANTE (CARD): */}
       <div className="flex w-full max-w-5xl bg-white rounded-3xl shadow-2xl overflow-hidden h-auto md:min-h-[600px]">
         {/* SECCIÓN IZQUIERDA: IMAGEN / BRANDING */}
