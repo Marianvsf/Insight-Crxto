b5d3a4bbeb14cb04c3558ad1c080f80bf0f35c81
Marianvsf <marianvsf@gmail.com>
2025-12-23 22:16:03 -0400

    top

--- src/components/userBalances.jsx
+++ src/components/userBalances.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useMemo, useCallback } from "react";
 import { getMockUsers } from "../mocks/mockUsers.js";
 import { CryptoSwapForm } from "./cryptoSwap.jsx";
+import TopMovers from "./topMovers.jsx";
 
 const URL_BASE = "https://api.coingecko.com/api/v3";
 const API_KEY = "&x_cg_demo_api_key=CG-qpB7vSSJxz2hyL8M2QWJfZrS";
@@ -235,6 +236,11 @@ const UserBalances = ({ userId }) => {
           </tbody>
         </table>
       </div>
+      {cryptoMarketData.length > 0 && (
+        <div className="my-8">
+          <TopMovers coins={cryptoMarketData} />
+        </div>
+      )}
     </div>
   );
 };
