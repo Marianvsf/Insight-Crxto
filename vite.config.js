318d83ec2f287b4f285f514088fd7c8d368c54c6
Marianvsf <marianvsf@gmail.com>
2026-07-03 23:04:25 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/insight-crxto/",
+  base: "/Insight-Crxto/",
 });
