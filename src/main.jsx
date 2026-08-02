ac493e3db2363e5ebd6401e2060562740d923dd6
Marianvsf <marianvsf@gmail.com>
2026-08-01 23:58:23 -0400

    page mini dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
+import { queryClient } from "./lib/queryClient.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
 );
